725f8e880c729afb07123e0718dc92b13c13f1c1
Chris <chris_legaxy@yahoo.com>
2020-03-27 21:06:19 +0700

    Added fonts and images

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,108 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 75,
+                backgroundColor: Colors.blueGrey,
+                backgroundImage: AssetImage('images/Chris.jpg'),
+              ),
+              Text(
+                'Chris',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20,
+                  color: Colors.white,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.white,
+                ),
+                margin: EdgeInsets.fromLTRB(35, 15, 35, 20),
+                padding: EdgeInsets.all(15),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      size: 25,
+                      color: Colors.teal[900],
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      '+855 017 70 17 15',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20,
+                        color: Colors.teal[900],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: Colors.white),
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 35,
+                ),
+                padding: EdgeInsets.all(15),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.mail,
+                      size: 25,
+                      color: Colors.teal[900],
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      'chris_legaxy@yahoo.com',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20,
+                        color: Colors.teal[900],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
